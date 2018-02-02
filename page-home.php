aed12e07fb9f25b4f3fd44af5f3356cff8719c3a
Josh Armentano <josh@abidewebdesign.com>
2018-02-01 16:04:27 -0800

    Updated home page post list code.

--- page-home.php
+++ page-home.php
@@ -22,20 +22,7 @@ $args = array(
 							<?php $query = new WP_Query($args); ?>
 							
 							<?php while ($query->have_posts()): $query->the_post(); ?>
-								<?php $image = get_field("featured_image"); ?>
-								<div class="posts-wrap row">
-									<div class="col-sm-3">
-										<a href="<?php the_permalink(); ?>"><img src="<?php echo $image['sizes']['post-thumb']; ?>" class="img img-responsive img-circle" /></a>
-									</div>
-									<div class="col-sm-9">
-										<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-										<div class="post-meta">
-											<time datetime="<?php echo get_the_date('c'); ?>" itemprop="datePublished"><?php echo get_the_date(); ?></time>
-										</div
-										<p><?php the_field("post_excerpt"); ?></p>
-										<a class="read-link" href="<?php the_permalink(); ?>">Read More</a>
-									</div>
-								</div>
+								<?php get_template_part('template-parts/content', 'list-post'); ?>
 							<?php endwhile; ?>
 							<div class="posts-wrap-footer row">
 								<div class="col-sm-12 text-center">
